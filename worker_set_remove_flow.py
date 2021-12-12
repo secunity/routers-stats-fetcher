1e690ac27b4b7c3d778144b70cb9182888a0ce22
gilad <igilfu@gmail.com>
2021-12-12 20:48:29 +0200

    small fixex

--- worker_set_remove_flow.py
+++ worker_set_remove_flow.py
@@ -86,8 +86,7 @@ if __name__ == '__main__':
     argsparse_params = {_: True for _ in ('host', 'port', 'username', 'password', 'key_filename',
                                           'vendor', 'command_prefix', 'log', 'url', 'dump')}
     args = initialize_start(argsparse_params=argsparse_params,
-                            module = False)
-                            # module = __file__.split('/')[-1].split('.')[0])
+                            module = __file__.split('/')[-1].split('.')[0])
 
     start_scheduler(start=True)
     add_job(func=_work, interval=15, func_kwargs=args, next_run_time=datetime.timedelta(seconds=2))
