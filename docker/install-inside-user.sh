06ad5f74daf0cfb3b471771ec9c2a46f916b6c0c
gilad <igilfu@gmail.com>
2021-12-14 14:32:41 +0200

    work on supervisor

--- docker/install-inside-user.sh
+++ docker/install-inside-user.sh
@@ -9,8 +9,10 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 rm -rf /opt/worker_statistics/ /opt/worker_set_remove_flow
 mkdir /opt/worker_statistics
 mkdir /opt/worker_set_remove_flow
+mkdir /opt/worker_sync_flows
 git clone git@github.com:secunity/routers-stats-fetcher.git /opt/worker_statistics
 git clone git@github.com:secunity/routers-stats-fetcher.git /opt/worker_set_remove_flow
+git clone git@github.com:secunity/routers-stats-fetcher.git /opt/worker_sync_flows
 
 python3 -m pip install --upgrade pip setuptools
 python3 -m pip install --upgrade virtualenv
@@ -26,6 +28,16 @@ pip install -r requirements.txt
 deactivate
 
 cd /opt/worker_set_remove_flow
+git checkout $SECUNITY_BRANCH
+python3 -m virtualenv venv
+source venv/bin/activate
+pip install --upgrade pip setuptools
+git fetch --all
+git pull
+pip install -r requirements.txt
+deactivate
+
+cd /opt/worker_sync_flows
 git checkout $SECUNITY_BRANCH
 python3 -m virtualenv venv
 source venv/bin/activate
