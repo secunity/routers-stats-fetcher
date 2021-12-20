992c6f3775f6fd11c810796e4e2a76a0fcb98dca
gilad <igilfu@gmail.com>
2021-12-20 12:31:02 +0200

    fix file_path in log

--- common/health_check.py
+++ common/health_check.py
@@ -20,7 +20,7 @@ def check_lines_size(lines, file_path):
 
 def read_health_check_files(logfile_path: str, ok_health_check: str, error_health_check: str, **kwargs):
     def check_file(file_name: str):
-        file_path = f'{logfile_path}{file_name}'
+        file_path = f'{logfile_path}/{file_name}'
         if os.path.isfile(file_path):
             with open(file_path, 'r', encoding='utf-8') as f:
                 lines = f.readlines()
@@ -36,7 +36,7 @@ def read_health_check_files(logfile_path: str, ok_health_check: str, error_healt
 
 def after_request(url_full_path: str, success:bool, logfile_path: str, ok_health_check: str, error_health_check: str, **kwargs):
     file_name = ok_health_check if success else error_health_check
-    file_path = f'{logfile_path}{file_name}'
+    file_path = f'{logfile_path}/{file_name}'
     add_line(f'{url_full_path},{datetime.datetime.utcnow().strftime(HEALTH_CHECK.FORMAT_TIME)}', file_path)
 
 
